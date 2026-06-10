62872e5e6df5faec6a483204338e615fc847e2a1
Zain Badaruddin <zainbadar24@gmail.com>
2026-06-10 06:39:33 +0500

    My first commit after project setup

---
+++ fraud-detection-service/fraud-detection-service/src/main/java/com/zain/fraud/fraud_detection_service/FraudDetectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zain.fraud.fraud_detection_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-detection-service/fraud-detection-service/src/test/java/com/zain/fraud/fraud_detection_service/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zain.fraud.fraud_detection_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
